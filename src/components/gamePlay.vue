71d8d603f168e4cb90213cdfbc80fb5ddff9eea4
Riyo <riyo.perry@gmail.com>
2022-03-10 17:08:02 -0800

    logic for checking guess against number combination added to gamePlay component - still has bugs

--- src/components/gamePlay.vue
+++ src/components/gamePlay.vue
@@ -13,8 +13,9 @@
                         <button id="guessSubmit" v-on:click="this.guessSubmit">Guess</button>
             </form>
         </section>
-        <section class="feedback">
+        <section id="viewFeedback" v-show="showFeedback">
             <h3>Feedback</h3>
+            <!-- how to show the current guess feedback? -->
             <p>{{this.feedback}}</p>
         </section>
         <section id="viewHistory" v-show="showHistory">
@@ -41,23 +42,61 @@ export default {
         guessNumber: 1,
         allGameGuesses: [],
         showHistory: false,
+        showFeedback: false
     };
   },
   methods: {
     guessSubmit: function() {
         let guessCombination = document.getElementById("guess").value;
         let guess = new Guess(this.guessNumber, guessCombination, "", "test");
-        
-        this.numberCombination.forEach(num => {
+        let correctNumLocations = 0;
+        let correctNumbers = 0;
+
         // checking guess against the number combination and deciding feedback
 
-        // after giving feedback, feedback is added to the Guess
-        // guess.guessFeedback = feedback;
-        });
+        for (var i = 0; i <= 3; i++) {
+            // checking  if number and location is correct
+            if (this.numberCombination[i] == guessCombination[i]) {
+                correctNumLocations += 1;
+            }
+        }
 
+        if(correctNumLocations == 0) {
+            // checking if any  of the guess numbers match with any of the number combination numbers
+             this.numberCombination.forEach(cNum => {
+                guessCombination.forEach(gNum => {
+                    if(cNum == gNum) {
+                        correctNumbers += 1;
+                    }
+                })
+            });
+        }
+
+        // feedback is added to the Guess
+        if (correctNumLocations > 0 ) {
+            guess.guessFeedback = `The player had guessed ${correctNumLocations} correct numbers and its correct location`;
+        } else if (correctNumbers > 0 ){
+            guess.guessFeedback = `The player has guessed ${correctNumbers} correct numbers`;
+        } else {
+            guess.guessFeedback = "The player’s guess was incorrect";
+        }
+
+        // the guess is added to the array of guesses for the game so that the user can view their game history
         this.allGameGuesses.push(guess);
         this.guessNumber++;
+        // the number of guesses remaining is lowered
         this.testGame.gameGuessesRemaining -= 1;
+
+        // Feedback is shown for guess is displayed to user
+        this.viewFeedback();
+
+        // correct number and location variable and currect number variable is reset to 0 for next guess
+        correctNumLocations = 0;
+        correctNumbers = 0;
+    },
+
+    viewFeedback: function() {
+        this.showFeedback = true;
     },
 
     viewHistory: function() {
